d418526e5b93d907b3e13a3f2cb4e85a308e983e
zhangjianhong <jhzhang11@iflytek.com>
2019-05-07 21:32:41 +0800

    1、管理输出；2、开发

---
+++ webpackDemo/4development/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configutration files as a base
+app.use(webpackDevMiddleware(compiler,{
+    publicPath:config.output.publicPath
+}))
+
+
+app.listen(3000,function () {
+    console.log("Example app listing on port 3000!\n");
+})
