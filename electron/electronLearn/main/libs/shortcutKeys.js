af4094e1996e8bd2f9555378cf0a36b0dcb9a090
zhangjianhong <jhzhang11@iflytek.com>
2019-10-18 09:15:22 +0800

    1、修改快捷键代码

--- electron/electronLearn/main/libs/shortcutKeys.js
+++ electron/electronLearn/main/libs/shortcutKeys.js
@@ -10,9 +10,9 @@ class Shortcut {
     register(keys="Command+Control+Alt+F5") {
         globalShortcut.register(keys,function() {
             let allWindow = BrowserWindow.getAllWindows();
-            for (let index =0; index < allWindow.length; index++) {
+            for (let index = 0; index < allWindow.length; index++) {
                 let win = allWindow[index];
-                if (win.webContents && !win.webContents.isDevToolsFocused()) {
+                if (win.webContents && !win.webContents.isDevToolsOpened()) {
                     win.webContents.openDevTools({
                         mode:'detach'
                     })
