e9a2b5bcc5add316975620b593e51f6da6ee8db8
zhangjianhong <jhzhang11@iflytek.com>
2019-10-21 09:17:21 +0800

    1、学习阶段保存

--- electron/electronLearn/main/main.js
+++ electron/electronLearn/main/main.js
@@ -1,52 +1,37 @@
-const {app,BrowserWindow} = require('electron');
-let shortcut = require('./libs/shortcutKeys.js'); //注册快捷键
+const {
+    app,
+    BrowserWindow,
+    ipcMain
+} = require('electron');
 
-// 保持对window对象的全局引用。如果不这么做的话，当Javascript对象被垃圾回收的时候，window对象将会自动的关闭
-let win;
-function createWindow() {
-    // 创建浏览器窗口
-    win = new BrowserWindow({
-        windth:800,
-        height:600,
+const shortCut = require('./libs/shortcutKeys.js'); 
+const path = require('path');
+
+let onlineStatusWindow;
+
+app.setUserTasks([
+    {
+        program:process.execPath,
+        arguments:'--new-window',
+        iconPath:process.execPath,
+        iconIndex:0,
+        title:'New Window',
+        description:'Create a new window'
+    }
+])
+app.on('ready',() => {
+    shortCut.register();
+    onlineStatusWindow = new BrowserWindow({
         webPreferences:{
             nodeIntegration:true
         }
-    })
-
-    // 加载index.html文件
-    win.loadFile('index.html');
-
-    shortcut.register();
-
-    // 当window被关闭，这个事件会被触发
-    win.on('closed',function () {
-        // 取消引用 window 对象，如果你的应用支持多窗口的话。通常会把多个 window 对象存放在一个数组里面，
-        // 与此同时，你应该删除对应的元素
-        win = null;
-    })
-}
-
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数
-// 部分 API 在 ready 事件触发后才能使用
-app.on('ready',createWindow);
-
-// 当全部窗口关闭时退出。
-app.on('window-all-closed',function () {
-    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出
-    // 否则绝大部分应用及其菜单栏会保持激活。
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    });
+    // onlineStatusWindow.loadURL(`file://${__dirname}/../online-status.html`);    
+    onlineStatusWindow.loadFile(`online-status.html`);
+    onlineStatusWindow.once('focus', () => onlineStatusWindow.flashFrame(false))
+    onlineStatusWindow.flashFrame(true)
 })
 
-app.on('activate',function () {
-    // 在 macOS 上，当单机dock图标并且没有其他窗口打开时，
-    // 通常在应用程序中重新创建一个窗口。
-    if(win === null) {
-        createWindow();
-    }
+ipcMain.on('online-status-change',(event,status) => {
+    console.log(status);
 })
-
-// 在这个文件中，你可以续写应用剩下的主进程代码。
-// 也可以拆分成几个文件，然后用 require 导入
