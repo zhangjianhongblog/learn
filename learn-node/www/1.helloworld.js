76a92a462deeeffe33a2f5316d1b86a16cc77b6a
zhangjianhong <jhzhang11@iflytek.com>
2019-11-11 09:12:53 +0800

    1、学习保存

--- learn-node/www/1.helloworld.js
+++ learn-node/www/1.helloworld.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const path = require('path');
+
+let tempStr = '.hhhh';
+console.log(__dirname);
+console.log(__filename);
+console.log(path.dirname(__filename))
+
+
+
+
+
+
+
+
+
+
+
