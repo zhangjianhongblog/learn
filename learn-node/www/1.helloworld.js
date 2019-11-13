231e24c88867d9163196566a0fed81c5ce3b5459
zhangjianhong <jhzhang11@iflytek.com>
2019-11-13 18:27:29 +0800

    1、学习保存

--- learn-node/www/1.helloworld.js
+++ learn-node/www/1.helloworld.js
@@ -1,17 +1,16 @@
-const path = require('path');
+console.log(1);
+new Promise((res,rej) =>{
+    console.log(1111);
+    res();
+}).then(() => {
+    console.log(2222);
+})
+setTimeout(() => {
+    console.log(2)
+}, 10);
+console.log(3);
+for (let i=0; i < 99999; i++) {}
 
-let tempStr = '.hhhh';
-console.log(__dirname);
-console.log(__filename);
-console.log(path.dirname(__filename))
+console.log(4);
 
-
-
-
-
-
-
-
-
-
-
+// 1 1111 3 4 2222 2
