98e03a88c4d94bc4f40cbd3fe48682f10ab6c017
zhangjianhong <jhzhang11@iflytek.com>
2019-12-05 11:30:28 +0800

    1删除无用代码

---
+++ parcel/empty-project-babel-css-less-vue/src/modules/list.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        adasd
+        <div v-for="(item,index) in myList" :key="index">{{item}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        myList:{
+            required:true,
+            type:Array,
+            default(){
+                return [1,2,3];
+            }
+        }
+    },
+    name:'',
+    components:{},
+    computed:{},
+    data(){
+        return {}
+    },
+    methods:{},
+    mounted(){},
+    watch:{}
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
