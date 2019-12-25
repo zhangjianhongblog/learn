59d4570e9fd2811d713ddad70e8892f47f22a1ce
zhangjianhong <jhzhang11@iflytek.com>
2019-12-25 17:11:05 +0800

    1、学习

--- parcel/empty-project-babel-css-less-vue/src/App.vue
+++ parcel/empty-project-babel-css-less-vue/src/App.vue
@@ -3,6 +3,8 @@
     <FunctionButton :title="'我是一个按钮'" @click="log"><span>哈哈哈哈</span></FunctionButton>
     <list :myList="undefined"></list>
     <slotLearn></slotLearn>
+    <base-input v-model="msg" :label.sync="myLabel" @blur="blurFn" name="张建红" v-bind.sync="myDoc">
+      <base-input>
   </div>
 </template>
 
@@ -10,20 +12,32 @@
   import FunctionButton from './modules/functionButton.js';
   import list from './modules/list.vue';
   import slotLearn from './modules/slotLearn.vue';
+  import baseInput from './modules/baseInput.vue';
   export default {
     data: function () {
       return {
         name: 'Hello World! 溺水时啥123123',
+        msg: "",
+        myLabel:'我是label',
+        myDoc:{
+          a:3,
+          c:2,
+          myName:'zhangjianhong'
+        }
       }
     },
     components: {
       FunctionButton,
       list,
-      slotLearn
+      slotLearn,
+      baseInput
     },
     methods: {
       log() {
         console.log('哈哈哈哈')
+      },
+      blurFn() {
+        console.log('我是去了焦点')
       }
     }
   };
---
+++ parcel/empty-project-babel-css-less-vue/src/modules/baseInput.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="baseInput">
+        <label>
+            {{label}}
+            <input v-bind="$attrs"
+                :value="value"
+                v-on="inputListeners">
+        </label>
+        <Button @click="clickMe">点击我</Button>
+        {{myName}}
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs:false,
+    props:['label','value','myName'],
+    name:'',
+    components:{},
+    computed:{
+        inputListeners(){
+            let vm = this;
+            return Object.assign({},vm.$listeners,{
+                input(event) {
+                    vm.$emit('input',event.target.value)
+                }
+            })
+        }
+    },
+    data(){
+        return {}
+    },
+    methods:{
+        clickMe(){
+            this.$emit('update:label','你是不是傻');
+            this.$emit('update:a',123);
+            this.$emit('update:myName',123);
+        }
+    },
+    mounted(){},
+    watch:{}
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
